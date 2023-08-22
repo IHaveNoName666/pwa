c60c7b412f7ded014779646c9fcb3028c3083c13
FunkaZan <U26895@rts-365.dk>
2023-08-22 13:46:28 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import PWAPrompt from 'react-ios-pwa-prompt'
+import PWAPrompt from "react-ios-pwa-prompt"
+import { Link } from 'react-router-dom';
 
 
 function App() {
-  return (  
+  return (
     <>
-    <header>This is header</header>
-
-    <main>
-        <Outlet/>
-    </main>
-
-    <footer>
-        <p>Do it propely!!</p>
-    </footer>
-
-    <PWAPrompt copyTitle="Føj til hjemmeskærm"/>
+      <header>
+        <p>Sej header</p>
+        <nav>
+          <Link to="/">Home</Link>&nbsp;&nbsp;
+          <Link to="/location">Location</Link>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <p>Sej footer</p>
+      </footer>
+      <PWAPrompt copyTitle="Føj til hjemmeskærm"/>
     </>
   );
 }
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -3,27 +3,58 @@ import { useState, useEffect } from "react";
  
 const Location = () => {
     const [myPos, setMyPos] = useState();
+    const [dynPos, setDynPos] = useState();
+    const [ipAddress, setIpAddress] = useState("");
 
+    // Get user's ip adress
+    useEffect(() => {
+      const fetchIp = async () => {
+        try {
+          const response = await fetch("https://api.ipify.org?format=json");
+          const data = await response.json();
+          setIpAddress(data.ip);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchIp();
+    }, []);
+
+ 
     useEffect(() => {
         if('geolocation' in navigator) {
             navigator.geolocation.getCurrentPosition((position) => {
                 setMyPos(position)
-                console.log(position)
             })
         }
     }, []);
 
+ 
+    const handleDynamicPosition = (position) => {
+        setDynPos(position)
+    }
+
+    const handleDynamicPositionError = (err) => {
+        console.log(err)
+    }
+
+    navigator.geolocation.watchPosition(handleDynamicPosition, handleDynamicPositionError, {timeout: 5000})
+
     return (
         <>
-            <h1>Location page</h1>
+            <p>Location page</p>
 
             <p>Latitude: {myPos?.coords.latitude}</p>
             <p>Longitude: {myPos?.coords.longitude}</p>
-            <p>Altitude: {myPos?.coords.altitude}</p>
-            <p>speed: {myPos?.coords.speed}</p>
+            <p>Speed: {myPos?.coords.speed}</p>
             <p>Accuracy: {myPos?.coords.accuracy}</p>
+            <p>Your ip adress: {ipAddress}</p>
 
-
+            <h1>This is dynamic position</h1>
+            <p>Latitude: {dynPos?.coords.latitude}</p>
+            <p>Longitude: {dynPos?.coords.longitude}</p>
+            <p>Speed: {dynPos?.coords.speed}</p>
+            <p>Accuracy: {dynPos?.coords.accuracy}</p>
         </>
      );
 }
